d70c90db19655186eb9853acc0a5e978c31efeba
Macksingh1 <malhanmack@google.com>
2024-11-08 23:36:23 +1100

    Initial Commit

---
+++ Clockin/ClockinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockinApp.swift
+//  Clockin
+//
+//  Created by Mack Singh on 8/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClockinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClockinTests/ClockinTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClockinTests.swift
+//  ClockinTests
+//
+//  Created by Mack Singh on 8/11/2024.
+//
+
+import Testing
+
+struct ClockinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
